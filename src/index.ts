3a477da18d7e07a8aa614d17b61d78bb7edaa46f
Rémi Lapeyre <remi.lapeyre@henki.fr>
2019-02-12 14:59:27 +0100

    Add get for all resources

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const port = 3000
 
 const sequelize = new Sequelize('postgres://user:pass@db:5432/db');
 
+
+// Models definition
+
 // 'post' will be the name of the table in the model
 // you can get the full documentation of model definition fields at
 // http://docs.sequelizejs.com/manual/tutorial/models-definition.html#data-types
@@ -23,10 +26,10 @@ const User = sequelize.define('user', {
 
 const Post = sequelize.define('post', {
     title: {
-      type: Sequelize.STRING
-    },
+        type: Sequelize.STRING
+      },
     content: {
-      type: Sequelize.STRING
+        type: Sequelize.STRING
     }
 });
 
@@ -52,22 +55,32 @@ const Commentaire = sequelize.define('comment', {
 });
 
 
-    Post.findAll().then(function(posts) {
-        let result = [];
+// Views
 
-        for (let post of posts) {
-            result.push({
-                title: post.title,
-                content: post.content
-            });
-        }
+function get(model, attributes) {
+    return function(request, response) {
+        model.findAll().then(function(objects) {
+            let result = [];
 
-        response.json(result);
-        // or set the status with:
-        // response.status(404).json(result);
-    });
+            for (let obj of objects) {
+                let res = new Object();
+                for (let attribute of attributes) {
+                    res[attribute] = obj[attribute];
+                }
 
-});
+                result.push(res);
+            }
+
+            response.json(result);
+        })
+    }
+}
+
+app.get('/v1/users', get(User, ["username", "email"]));
+app.get('/v1/posts', get(Post, ["title", "content"]));
+app.get('/v1/tags', get(Tag, ["name"]));
+app.get('/v1/images', get(ImagePost, ["url", "description"]));
+app.get('/v1/comments', get(Commentaire, ["content"]));
 
 app.listen(port, function() {
     console.log(`Your app is listening on ${port}!`);
