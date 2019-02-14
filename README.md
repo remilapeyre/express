ea360a0269031d2ec04af7252e020f565e211c6b
Rémi Lapeyre <remi.lapeyre@henki.fr>
2019-02-14 23:55:43 +0100

    Add information about DELETE

--- README.md
+++ README.md
@@ -1,46 +1,48 @@
-# What's next
-
-Now that our API returns a list of objects on
+Great, we now can list objects on:
 - `/v1/users`
 - `/v1/posts`
 - `/v1/tags`
 - `/v1/images`
 - `/v1/comments`
 
-We would like to be able to fetch the details of only one object at those URL:
+And fetch information about a specific object at:
 - `/v1/users/[id]`
 - `/v1/posts/[id]`
 - `/v1/tags/[id]`
 - `/v1/images/[id]`
 - `/v1/comments/[id]`
 
-Remember that with Express, you can create a new endpoint by doing:
+We also can create some new objects by sending a POST request at:
+- `/v1/users`
+- `/v1/posts`
+- `/v1/tags`
+- `/v1/images`
+- `/v1/comments`
+
+
+# What's next
+
+We would like to be able to destroy an object present in the database.
+
+You already know how to call a different function based the method of the incoming
+request with Express. You need to call a knew function for DELETE methods.
+
+If you don't remember how to do it, look at the precedent exercises or read the
+documentation at https://expressjs.com/en/guide/routing.html
+
+
+When the client wants to delete something, he will need to indicate the id of the
+object he wants to destroy by indicating it in the route, like he did for fetching
+details about an object (e.g. `/v1/posts/1`, `/v1/posts/2`, etc.)
+
+You will first need to fetch the object from the database (remember to return an
+error 404 if there is nothing with this id).
+
+Once you got the object you can call `.destroy()` to remove it from the database:
 ``` javascript
-app.get('/v1/my_new_route', function_to_call);
+// http://docs.sequelizejs.com/manual/tutorial/instances.html#destroying-deleting-persistent-instances
 
-// Take the time to read https://expressjs.com/en/guide/routing.html
-// for more information
-```
-
-You will need to know the id of the resource the user wants to call (`/v1/posts/1`,
-`/v1/posts/2`, ...).
-
-To do so, you can use **route parameters** (https://expressjs.com/en/guide/routing.html#route-parameters)
-like so:
-``` javascript
-app.get('/v1/posts/:id', function(request, response) {
-    // the id in the route will be accessible at:
-    // request.params.id
+obj.destroy().then(function() {
+    // the object has been removed
 })
 ```
-
-Once you know the id you need to fetch, you can get it from Sequelize with
-the `findById` method (http://docs.sequelizejs.com/manual/tutorial/models-usage.html#-find-search-for-one-specific-element-in-the-database):
-``` javascript
-Post.findById(my_id).then(function(post) {
-    // post will be the object from the database
-    // if the id did not exist, post will be `null`
-})
-```
-
-Remember that if an id does not exists, you must return an error 404.
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const port = 3000
 
 const sequelize = new Sequelize('postgres://user:pass@db:5432/db');
 
+app.use(express.json())
+
 
 // Models definition
 
@@ -54,8 +56,21 @@ const Commentaire = sequelize.define('comment', {
     }
 });
 
+// Helper functions
 
-// Views
+// Take an object and a list of attributes and return the payload to send to
+// the client
+function formatResult(obj, attributes) {
+    let res = Object();
+
+    for (let attribute of attributes) {
+        res[attribute] = obj[attribute];
+    }
+
+    return res;
+}
+
+// Views - we are going to implement GET, PUST, POST and DELETE
 
 function get(model, attributes) {
     return function(request, response) {
@@ -63,12 +78,7 @@ function get(model, attributes) {
             let result = [];
 
             for (let obj of objects) {
-                let res = new Object();
-                for (let attribute of attributes) {
-                    res[attribute] = obj[attribute];
-                }
-
-                result.push(res);
+                result.push(formatResult(obj, attributes));
             }
 
             response.json(result);
@@ -81,6 +91,59 @@ app.get('/v1/posts', get(Post, ["id", "title", "content"]));
 app.get('/v1/tags', get(Tag, ["id", "name"]));
 app.get('/v1/images', get(ImagePost, ["id", "url", "description"]));
 app.get('/v1/comments', get(Commentaire, ["id", "content"]));
+
+
+function getOne(model, attributes) {
+    return function(request, response) {
+        // We must fetch the object from the database using it's id
+        // http://docs.sequelizejs.com/manual/tutorial/models-usage.html#-find-search-for-one-specific-element-in-the-database
+        model.findById(request.params.id).then(function(obj) {
+            if (obj != null) {
+                // If an object is found, we format the response and return it
+                // to the client
+                response.json(formatResult(obj, attributes));
+            } else {
+                // If we did not find anything, we return an error
+                response.status(404).json({"error": "Not found"});
+            }
+        })
+    }
+}
+
+app.get('/v1/users/:id', getOne(User, ["id", "username", "email"]));
+app.get('/v1/posts/:id', getOne(Post, ["id", "title", "content"]));
+app.get('/v1/tags/:id', getOne(Tag, ["id", "name"]));
+app.get('/v1/images/:id', getOne(ImagePost, ["id", "url", "description"]));
+app.get('/v1/comments/:id', getOne(Commentaire, ["id", "content"]));
+
+
+
+function create(model, attributes) {
+    return function(request, response) {
+
+        // We get the fields from the request body
+        let data = Object();
+        for (let attribute of attributes) {
+            data[attribute] = request.body[attribute];
+        }
+
+        // We create a new object
+        // http://docs.sequelizejs.com/manual/tutorial/instances.html#building-a-non-persistent-instance
+        let obj = model.build(data);
+
+        // Save it
+        obj.save().then(function(result) {
+            response.json(formatResult(result, attributes));
+        })
+    }
+}
+
+app.post('/v1/users/:id', create(User, ["id", "username", "email"]));
+app.post('/v1/posts/:id', create(Post, ["id", "title", "content"]));
+app.post('/v1/tags/:id', create(Tag, ["id", "name"]));
+app.post('/v1/images/:id', create(ImagePost, ["id", "url", "description"]));
+app.post('/v1/comments/:id', create(Commentaire, ["id", "content"]));
+
 
 app.listen(port, function() {
     console.log(`Your app is listening on ${port}!`);
